088e9fe89b1eada4c2365222a9c6140794ed8020
BillMagou <throwsasori@gmail.com>
2026-08-16 13:24:46 +0300

    Support cinema search query

--- backend/controllers/theatreController.js
+++ backend/controllers/theatreController.js
@@ -2,7 +2,7 @@ const theatreService = require("../services/theatreService");
 
 const getTheatres = async (req, res) => {
     try {
-        const theatres = await theatreService.getAllTheatres();
+        const theatres = await theatreService.getAllTheatres(req.query.search || "");
         res.json(theatres);
     } catch (error) {
         res.status(500).json({ error: error.message });
