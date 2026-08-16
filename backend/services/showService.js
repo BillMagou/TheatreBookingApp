b79fb563d2672f14a0328e654c50084c1a1a2d9c
BillMagou <throwsasori@gmail.com>
2026-08-16 13:24:53 +0300

    Filter movies by cinema and include cinema details

--- backend/services/showService.js
+++ backend/services/showService.js
@@ -1,8 +1,30 @@
 const pool = require("../config/db");
 
-const getAllShows = async () => {
-    const rows = await pool.query("SELECT * FROM shows");
-    return rows;
+const getAllShows = async (theatreId) => {
+    let sql = `
+        SELECT
+            s.show_id,
+            s.theatre_id,
+            s.title,
+            s.show_time,
+            s.available_seats,
+            t.name AS theatre_name,
+            t.location AS theatre_location,
+            t.description AS theatre_description
+        FROM shows s
+        JOIN theatres t ON t.theatre_id = s.theatre_id
+    `;
+
+    const params = [];
+
+    if (theatreId) {
+        sql += " WHERE s.theatre_id = ?";
+        params.push(theatreId);
+    }
+
+    sql += " ORDER BY s.show_time, s.title";
+
+    return await pool.query(sql, params);
 };
 
 module.exports = {
